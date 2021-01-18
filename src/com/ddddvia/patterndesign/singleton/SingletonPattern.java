34955ac65dace9e3ca466861551d6cd83f04be71
ddddvia <hedongwei0713@163.com>
2021-01-18 16:08:26 +0800

    创建设计模式目录

---
+++ src/com/ddddvia/patterndesign/singleton/SingletonPattern.java
@@ -0,0 +1,12 @@
+package com.ddddvia.patterndesign.singleton;
+
+/**
+ * @Author: hedwf
+ * @Date:
+ * @Version: 1.0
+ */
+public class SingletonPattern {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
